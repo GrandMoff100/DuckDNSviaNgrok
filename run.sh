5868d2f23fe24e616b4c7f10887d90bc13a39927
Nate <minecraftcrusher100@gmail.com>
2021-07-11 01:58:22 +0000

    Fixed cmdline args to pass variables instead of strings

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ DUCKDNS_TOKEN=$(bashio::config 'duckdns_token')
 DUCKDNS_DOMAINS=$(bashio::config 'duckdns_domains')
 
 pip install -r requirements.txt
-python main.py NGROK_TOKEN DUCKDNS_TOKEN ${DUCKDNS_DOMAINS[@]}
+python3 main.py $NGROK_TOKEN $DUCKDNS_TOKEN ${DUCKDNS_DOMAINS[@]}
