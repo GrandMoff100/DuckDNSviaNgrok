2f871ede897893dd6fc56dfbe029907bf2eda751
Nate <minecraftcrusher100@gmail.com>
2021-07-11 01:52:36 +0000

    Waiting for process and certificate fix added

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,13 @@ ARG BUILD_FROM
 FROM $BUILD_FROM
 
 ENV LANG C.UTF-8
-RUN apk add --no-cache python3
+RUN apk add --no-cache python3 py3-pip
 
 # Copy data for add-on
 COPY run.sh /
+COPY requirements.txt /
+COPY main.py /
+
 RUN chmod a+x /run.sh
 
 CMD [ "/run.sh" ]
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pyngrok import ngrok
 file, ngrok_token, duckdns_token, *duckdns_domains = sys.argv
 
 ngrok.set_auth_token(ngrok_token)
-tunnel = ngrok.connect(8123)
+tunnel = ngrok.connect(8123, insecure=True)
 print('Ngrok URL:',  tunnel.public_url)
 protocol, domain = tunnel.public_url.split('://')
 ip = socket.gethostbyname(domain)
@@ -22,3 +22,39 @@ PARAMS = {
 resp = requests.get(BASE, params=PARAMS)
 print('DuckDNS URL:', resp.url)
 print('DuckDNS Response:', resp.text)
+import sys
+import socket
+import requests
+from pyngrok import ngrok
+
+file, ngrok_token, duckdns_token, *duckdns_domains = sys.argv
+
+ngrok.set_auth_token(ngrok_token)
+tunnel = ngrok.connect(8123, insecure=True)
+print('Ngrok URL:',  tunnel.public_url)
+
+
+protocol, domain = tunnel.public_url.split('://')
+ip = socket.gethostbyname(domain)
+print('Resolved IP:', ip)
+
+BASE = 'https://www.duckdns.org/update'
+PARAMS = {
+    'token': duckdns_token,
+    'ip': ip,
+    'domains': ','.join(duckdns_domains)
+}
+
+resp = requests.get(BASE, params=PARAMS)
+print('DuckDNS URL:', resp.url)
+print('DuckDNS Response:', resp.text)
+
+ngrok_process = ngrok.get_ngrok_process()
+
+try:
+    # Block until CTRL-C or some other terminating event
+    print('Waiting for tunnel process completion.')
+    ngrok_process.proc.wait()
+except KeyboardInterrupt:
+    print("Shutting down server.")
+    ngrok.kill()
